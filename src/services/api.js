5451b63a42bb175ff08171175f07f3d7190f65ea
Sadko-o <ruana.saduakhas@nu.edu.kz>
2024-06-24 01:40:54 +0500

    add: comment

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,4 @@ export const fetchCurrencyData = async (date) => {
   const response = await axios.get(`/api/rss/get_rates.cfm?fdate=${date}`);
   return response.data;
 };
-
+// it sends a GET request to the API to fetch currency rates data for the specified date.
